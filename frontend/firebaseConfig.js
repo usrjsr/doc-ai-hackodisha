c8a9e071bf6007858eca158b9f74a8f6c6ef3bfe
usrjsr <utkarshsinghdau@gmail.com>
2025-09-06 04:01:29 +0530

    key's

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCepd23BbtPUB9koqSbcOp0t4WVCmQPEG4",
-  authDomain: "doc-ai-c204f.firebaseapp.com",
-  projectId: "doc-ai-c204f",
-  storageBucket: "doc-ai-c204f.firebasestorage.app",
-  messagingSenderId: "214381226149",
-  appId: "1:214381226149:web:bc90d58345fdbd76da854c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
 export const provider = new GoogleAuthProvider();
